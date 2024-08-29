e203a7b06e2a9cf23b900f93b9b6e4b4cf34470e
Azeem Brown <azeembrown717@gmail.com>
2024-08-28 22:17:31 -0400

    fixes

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,9 @@
 // add middlewares here related to actions
+const authentication = (req, res, next) => {
+    if(true){
+        next()
+    } else {
+        res.status(401).send('Unauthorized')
+    }
+}
+module.exports = authentication
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,10 +1,15 @@
 // Write your "actions" router here!
+
 const express = require('express');
+const authentication = require('./actions-middlware')
 const server = express();
 
+console.table({id: 1, name: 'Azeem'})
 server.use(express.json());
-server.get('/actions', (req, res) => {
+
+server.get('/', authentication, (req, res) => {
     res.send('hello from actions')
+    console.log('here')
 })
 
 module.exports = server
