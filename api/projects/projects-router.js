92e61adbc6d87fa26216da3fdd419d7e1d0fa9ec
Azeem Brown <azeembrown717@gmail.com>
2024-09-02 17:15:57 -0400

    16outOf27

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -18,4 +18,18 @@ server.get('/', async (req, res) => {
     }
 })
 
+server.get('/:id', async (req, res) => {
+    const { id } = req.params
+    try {
+        const projects = await get(id)
+        if (!projects) {
+            res.status(404).send()
+        } else {
+            res.status(200).json(projects)
+        }
+    } catch (err) {
+        res.status(500).json({ "status": "Not found" })
+    }
+})
+
 module.exports = server
