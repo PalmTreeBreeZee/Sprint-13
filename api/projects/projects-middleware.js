139864c5a72c90fe65ba1394721239b5e99814ba
Azeem Brown <azeembrown717@gmail.com>
2024-09-02 20:22:12 -0400

    25outOf27

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,26 @@
 // add middlewares here related to projects
+
+
+async function missingParts(req, res, next) {
+    const { body } = req
+    console.log(body)
+    if (!body.name || !body.description) {
+        res.status(400).send('Unauthorized')
+
+    } else {
+        res.status(200)
+        next()
+    }
+}
+
+async function missingParts2(req, res, next) {
+    const { body } = req
+    console.log(body.completed)
+    if (!body.name || !body.description || body.completed === undefined) {
+        res.status(400).send('Unauthorized')
+
+    } else {
+        next()
+    }
+}
+module.exports = { missingParts, missingParts2 }
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const { get, insert, update, remove, getProjectActions, } = require("./projects-model")
 const server = express()
-
+const { missingParts, missingParts2 } = require('./projects-middleware')
 server.use(express.json())
 
 server.get('/', async (req, res) => {
@@ -31,13 +31,53 @@ server.get('/:id', async (req, res) => {
         res.status(500).json({ "status": "Not found" })
     }
 })
-server.post('/', async (req, res) => {
+server.post('/', missingParts, async (req, res) => {
     try {
         const { body } = req
-        console.log(body)
         const projects = await insert(body)
         if (!projects) {
             res.status(400).send()
+        } else {
+            res.status(200).json(projects)
+        }
+    } catch (err) {
+        res.status(500).json({ "status": "Not found" })
+    }
+})
+server.put('/:id', missingParts2, async (req, res) => {
+    try {
+        const { id } = req.params
+        const { body } = req
+        const projects = await update(id, body)
+        if (!projects) {
+            res.status(400).send()
+        } else {
+            res.status(200).json(projects)
+        }
+    } catch (err) {
+        res.status(500).json({ "status": "Not found" })
+    }
+})
+server.delete('/:id', async (req, res) => {
+
+    try {
+        const { id } = req.params
+        const projects = await remove(id)
+        if (!projects) {
+            res.status(404).send()
+        } else {
+            res.status(200).json(projects)
+        }
+    } catch (err) {
+        res.status(500).json({ message: err.message })
+    }
+})
+server.get('/:id/actions', async (req, res) => {
+    const { id } = req.params
+    try {
+        const projects = await getProjectActions(id)
+        if (!projects) {
+            res.status(404).send()
         } else {
             res.status(200).json(projects)
         }
